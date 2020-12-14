40a51e443d3ccba1700abdcbc311d25069d904e4
dengli <1836610743@qq.com>
2020-12-14 18:47:03 +0800

    项目初始化文件完成，配置文件vue.config.js/.editorconfig完成

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+modules.exports = {
+    configureWebpack: {
+        resolve: {
+            //  别名配置
+            alias:{
+                'assets':'@assets',
+                'common':'@common',
+                'components':'@components',
+                'network':'@network',
+                'views':'@views',
+            }
+        }
+    }
+}
